d10b9b13e2797867e73408f1b766d8e806e27892
CS-Tao <whucstao@gmail.com>
2022-06-02 01:05:56 +0800

    📦 chore: init project

---
+++ .config/commitizen.js
@@ -0,0 +1,42 @@
+module.exports = {
+  types: [
+    {
+      value: '🚧 WIP',
+      name: '🚧  WIP:      Work in progress',
+    },
+    {
+      value: '🎸 feat',
+      name: '🎸  feat:     A new feature',
+    },
+    {
+      value: '🐛 fix',
+      name: '🐛  fix:      A bug fix',
+    },
+    {
+      value: '📦 chore',
+      name: "📦  chore:    Changes that don't modify src or test files. Such as updating build tasks, package manager",
+    },
+    {
+      value: '🧰 refactor',
+      name: '🧰  refactor: A code change that neither fixes a bug nor adds a feature',
+    },
+    {
+      value: '📚 docs',
+      name: '📚  docs:     Documentation only changes',
+    },
+    {
+      value: '🏁 test',
+      name: '🏁  test:     Add missing tests or correcting existing tests',
+    },
+    {
+      value: '🎨 style',
+      name: '🎨  style:    Code Style, Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)',
+    },
+    {
+      value: '⏪ revert',
+      name: '⏪  revert:   Revert to a commit',
+    },
+  ],
+  allowBreakingChanges: ['feat', 'fix'],
+  skipQuestions: ['scope', 'body', 'footer'],
+}

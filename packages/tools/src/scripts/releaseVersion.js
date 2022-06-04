27299719f50fc251d529c0a0f66204c17489bb6c
CS-Tao <whucstao@gmail.com>
2022-06-05 01:06:39 +0800

    📦 chore: add cache

--- packages/tools/src/scripts/releaseVersion.js
+++ packages/tools/src/scripts/releaseVersion.js
@@ -8,14 +8,19 @@ module.exports = function releaseVersion(bumpType) {
   let preid
   let options
   let bump
+  let publishOptions
   if (currentBranch === mainBranch) {
     preid = 'beta'
     options = '--no-commit-hooks'
     bump = bumpType || 'prerelease'
+    publishOptions = ['major', 'minor', 'patch'].includes(bumpType) ? '--dist-tag=beta' : '--dist-tag=latest'
   } else {
     preid = currentCommitHash.slice(0, 8)
     options = '--no-commit-hooks --no-git-tag-version --no-push'
     bump = 'prepatch'
+    publishOptions = '--dist-tag=alpha'
   }
   shell.exec(`yarn lerna version ${bump} --preid ${preid} ${options} --yes`)
+  shell.exec('yarn build')
+  shell.exec(`yarn lerna publish from-git ${publishOptions} --skip-git --yes`)
 }

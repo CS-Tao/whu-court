cc6e9fe014a09d36c666ce9c4d8025f080ee0aaa
CS-Tao <cstao.cn@icloud.com>
2022-06-16 12:28:38 +0800

    📦 chore: test sentry sm

--- packages/reporter/src/reporter.ts
+++ packages/reporter/src/reporter.ts
@@ -1,6 +1,8 @@
+import path from 'path'
 import address from 'address'
 import md5 from 'md5'
 import * as Sentry from '@sentry/node'
+import { RewriteFrames } from '@sentry/integrations'
 import '@sentry/tracing'
 import { environment, version } from '@whu-court/env'
 import Measure from './measure'
@@ -26,7 +28,12 @@ class Reporter {
     Sentry.init({
       dsn: 'https://9f5715bdde344fbd8ee3fb3696edfd86@o1268975.ingest.sentry.io/6505233',
       tracesSampleRate: 1.0,
-      integrations: [new Sentry.Integrations.Http({ tracing: true })], // TODO:
+      integrations: [
+        new Sentry.Integrations.Http({ tracing: true }), // TODO:
+        new RewriteFrames({
+          root: path.join('..', '..', process.cwd()),
+        }),
+      ],
       release: `whu-court@v${version}`,
       environment,
       sendClientReports: false,

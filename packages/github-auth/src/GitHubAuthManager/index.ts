e8f1be9c796b8bef713f3a2e8621b44a0efca765
CS-Tao <cstao.cn@icloud.com>
2022-06-22 13:26:31 +0800

    📦 chore: update packages

---
+++ packages/github-auth/src/GitHubAuthManager/index.ts
@@ -0,0 +1,29 @@
+import GitHubService from './GitHubService'
+
+class GitHubAuthManager extends GitHubService {
+  isAuthed: boolean = false
+
+  checkIfStared = async (token: string) => {
+    this.isAuthed = await this.checkIfStaredCore(token, null)
+    return this.isAuthed
+  }
+
+  private checkIfStaredCore = async (token: string, cursor: string | null): Promise<boolean> => {
+    try {
+      const data = await this.checkStared(token, cursor).then(([token, lastCursor, haveStarted]) => {
+        return {
+          continue: !haveStarted && !!lastCursor,
+          token,
+          haveStarted,
+          lastCursor,
+        }
+      })
+      return data.continue ? this.checkIfStaredCore(data.token, data.lastCursor) : data.haveStarted
+    } catch (error) {
+      // TODO:
+      throw new Error('todo')
+    }
+  }
+}
+
+export default GitHubAuthManager

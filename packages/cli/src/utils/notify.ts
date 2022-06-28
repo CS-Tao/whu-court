29d3f1a171ecde8452aed86282715aac206dd350
CS-Tao <whucstao@gmail.com>
2022-06-29 00:34:35 +0800

    🎸 feat: add config and notify

---
+++ packages/cli/src/utils/notify.ts
@@ -0,0 +1,17 @@
+import notifier from 'node-notifier'
+import path from 'path'
+
+const logo = path.join(__dirname, '../../assets/icon/logo.png')
+
+export default class Notify {
+  public static notify(title: string, message: string) {
+    notifier.notify({
+      title,
+      message,
+      icon: logo,
+      contentImage: logo,
+      wait: false,
+      timeout: false,
+    })
+  }
+}
